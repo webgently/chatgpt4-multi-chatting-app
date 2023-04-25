c2a9dc4e7e3734fb939273206c8e9684ee10604d
webgently <oleksandrshkabura811@gmail.com>
2023-04-25 05:26:28 +0300

    feat: initial skeleton of the project

---
+++ src/@types/global.d.ts
@@ -0,0 +1,11 @@
+declare module '.jpg'
+declare module '.svg'
+declare module '.png'
+
+declare module 'url:*' {
+    export default string
+}
+declare module '*.svg' {
+    const content: any
+    export default content
+}
