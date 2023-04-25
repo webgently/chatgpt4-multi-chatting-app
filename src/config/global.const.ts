7d8b3322a4d08c34fdfb9772083ec6d23ce8414c
webgently <oleksandrshkabura811@gmail.com>
2023-04-26 02:01:44 +0300

    feat: add config variables

--- src/config/global.const.ts
+++ src/config/global.const.ts
@@ -1,1 +1,7 @@
-export {}
+export const config = {
+  serverHost: process.env.REACT_APP_API_URL,
+  appKey: process.env.REACT_APP_APP_KEY,
+  tokenString:
+    JSON.parse(localStorage.getItem(`${process.env.REACT_APP_APP_KEY}`) as any)?.auth?.token ||
+    JSON.parse(sessionStorage.getItem(`${process.env.REACT_APP_APP_KEY}`) as any)?.auth?.token
+};
