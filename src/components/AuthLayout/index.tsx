8b31dbc6581ec99a9f1855cae7c6e3d0aeaa701c
webgently <oleksandrshkabura811@gmail.com>
2023-04-26 01:55:23 +0300

    feat: add auth layout component

--- src/components/AuthLayout/index.tsx
+++ src/components/AuthLayout/index.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useNavigate, useLocation, Outlet } from 'react-router-dom';
 
 const AuthLayout = (props: any) => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (location.pathname === '/') navigate('/login');
+  }, [location]);
+
   return (
     <div className="auth-layout">
-      <main>{props.children}</main>
+      <Outlet />
     </div>
   );
 };
