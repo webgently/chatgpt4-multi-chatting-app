7e8fc142bf18a3ddf72de03e364a7e984cc2eee5
webgently <oleksandrshkabura811@gmail.com>
2023-04-28 01:59:08 +0300

    update: config variable

--- src/config/global.const.ts
+++ src/config/global.const.ts
@@ -1,7 +1,7 @@
 export const config = {
   serverHost: process.env.REACT_APP_API_URL,
-  appKey: process.env.REACT_APP_APP_KEY,
+  appKey: process.env.REACT_APP_APP_TOKEN_KEY,
   tokenString:
-    JSON.parse(localStorage.getItem(`${process.env.REACT_APP_APP_KEY}`) as any)?.auth?.token ||
-    JSON.parse(sessionStorage.getItem(`${process.env.REACT_APP_APP_KEY}`) as any)?.auth?.token
+    JSON.parse(localStorage.getItem(`${process.env.REACT_APP_APP_TOKEN_KEY}`) as any)?.auth?.token ||
+    JSON.parse(sessionStorage.getItem(`${process.env.REACT_APP_APP_TOKEN_KEY}`) as any)?.auth?.token
 };
