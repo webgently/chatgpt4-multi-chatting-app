eb6468b6a6ce57ae31dd95eec973cd9c00e8fb5b
webgently <oleksandrshkabura811@gmail.com>
2023-04-27 23:53:14 +0300

    feat: structure the modal system

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
+import { ReactNotifications } from 'react-notifications-component';
+import 'react-notifications-component/dist/theme.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'animate.css';
+import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
+root.render(
+  <>
+    <ReactNotifications />
+    <App />
+  </>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
