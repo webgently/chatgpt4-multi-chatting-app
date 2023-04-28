7a6bf447640dc1d8e5397722a1f36524eff5a362
webgently <oleksandrshkabura811@gmail.com>
2023-04-28 08:51:17 +0300

    feat: add user layout component

---
+++ src/components/UserLayout/index.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react';
+import { useNavigate, useLocation, Outlet } from 'react-router-dom';
+import useStore from '../../useStore';
+import UserSidebar from '../UserSidebar';
+import UserList from '../UserList';
+
+const UserLayout = () => {
+  const { token } = useStore();
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!token) navigate('/login');
+    switch (location.pathname) {
+      case '/user/chatting':
+        break;
+    }
+  }, [location]);
+
+  return (
+    <div className="user-layout">
+      <UserSidebar />
+      <UserList />
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
+export default UserLayout;
