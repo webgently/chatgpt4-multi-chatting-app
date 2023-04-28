434bc3994911b86781219bd7fbb31ae389024533
webgently <oleksandrshkabura811@gmail.com>
2023-04-28 22:10:21 +0300

    update: function that get token

--- src/config/global.const.ts
+++ src/config/global.const.ts
@@ -2,6 +2,6 @@ export const config = {
   serverHost: process.env.REACT_APP_API_URL,
   appKey: process.env.REACT_APP_APP_TOKEN_KEY,
   tokenString:
-    JSON.parse(localStorage.getItem(`${process.env.REACT_APP_APP_TOKEN_KEY}`) as any)?.auth?.token ||
-    JSON.parse(sessionStorage.getItem(`${process.env.REACT_APP_APP_TOKEN_KEY}`) as any)?.auth?.token
+    JSON.parse(localStorage.getItem(`${process.env.REACT_APP_APP_TOKEN_KEY}`) as any)?.token ||
+    JSON.parse(sessionStorage.getItem(`${process.env.REACT_APP_APP_TOKEN_KEY}`) as any)?.token
 };
