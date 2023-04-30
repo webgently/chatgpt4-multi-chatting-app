0ba5541910f59c3e8cd4667516c4c66e25b24f8a
webgently <oleksandrshkabura811@gmail.com>
2023-04-30 15:51:45 +0300

    feat: add chat request type

--- src/type/index.ts
+++ src/type/index.ts
@@ -1,1 +1,11 @@
-export {}
+export interface MSGTypeInterface {
+  from: string;
+  to: string;
+  message: string[];
+  first_name: string;
+  last_name: string;
+  email: string;
+  user_name: string;
+  avatar: string;
+  date: Date;
+}
