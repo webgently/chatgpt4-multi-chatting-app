ad2f09e13039bfc6e0a245226bd57eec9a5f1687
webgently <oleksandrshkabura811@gmail.com>
2023-04-30 15:53:46 +0300

    feat: implement chatroom page

--- src/pages/ChatRoom/index.tsx
+++ src/pages/ChatRoom/index.tsx
@@ -1,7 +1,146 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
+import { io } from 'socket.io-client';
+import IconMenu from '../../components/Icons';
+import useStore from '../../useStore';
+import { getUserInfo } from '../../utils';
+import { MSGTypeInterface } from '../../type';
+import AIImg from '../../assets/images/openai.png';
+
+const socket = io(process.env.REACT_APP_BACKEND_BASE_URL as string);
 
 const ChatRoom = () => {
-  return <></>;
+  const { token } = useStore();
+  const [user] = useState<any>(getUserInfo(token));
+  const messagesEndRef: React.MutableRefObject<any> = useRef(null);
+  const inputRef: React.MutableRefObject<any> = useRef(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [msg, setMsg] = useState('');
+  const [allMsg, setAllMsg] = useState<MSGTypeInterface[]>([]);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  };
+  const handleKeyDown = async (e: any) => {
+    if (!e.shiftKey && e.keyCode === 13) {
+      if (!isLoading) {
+        setIsLoading(true);
+        await sendChatting();
+      }
+    }
+  };
+  const handleKeyUp = async (e: any) => {
+    if (!e.shiftKey && e.keyCode === 13) {
+      setMsg('');
+      inputRef.current.focus();
+    }
+  };
+  const sendChatting = async () => {
+    if (!msg) return;
+    const data: MSGTypeInterface = {
+      from: user.user_id,
+      to: 'chatgpt',
+      message: msg.split('\n'),
+      first_name: user.first_name,
+      last_name: user.last_name,
+      email: user.email,
+      user_name: user.user_name,
+      avatar: user.avatar,
+      date: new Date()
+    };
+    setAllMsg((prev) => [...prev, data]);
+    socket.emit('sent message to server', data);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    socket.on('connect', async () => {
+      const data = {
+        user_id: user.user_id,
+        group: user.group
+      };
+      /* ---------------join in rooms--------------- */
+      socket.emit('join room', data);
+    });
+
+    socket.on('group', (e) => {
+      setAllMsg((prev) => [...prev, e]);
+    });
+
+    socket.on('chatgpt', (e) => {
+      setAllMsg((prev) => [...prev, e]);
+    });
+
+    return () => {
+      socket.off('connect');
+      socket.off('group');
+    };
+    // eslint-disable-next-line
+  }, []);
+
+  useEffect(() => {
+    setTimeout(() => {
+      scrollToBottom();
+    }, 100);
+  }, [allMsg]);
+
+  return (
+    <div className="chatroom">
+      <div className="chatroom-header">
+        <div className="chatroom-element-info">
+          <h4>ChatGPT</h4>
+          <p>bot</p>
+        </div>
+      </div>
+      <div className="chatroom-body">
+        <div className="chatroom-msg-content">
+          <div className="chatroom-msg-group">
+            {allMsg.map((item: MSGTypeInterface, ind1: number) => {
+              return (
+                <div key={ind1} className={item.from === user.user_id ? 'justify-end' : 'justify-start'}>
+                  {item.from !== user.user_id && (
+                    <div className="user-detail">
+                      {item.user_name === 'GPT' ? (
+                        <img src={AIImg} alt="avatar" />
+                      ) : (
+                        <>
+                          {item.avatar ? <img src={item.avatar} alt="avatar" /> : <p>{item.user_name.slice(0, 1)}</p>}
+                        </>
+                      )}
+                    </div>
+                  )}
+                  <div className={`messages ${item.from === user.user_id ? 'bg-forth' : 'bg-second'}`}>
+                    {item.message.map((ele: string, ind2: number) => {
+                      return <p key={ind1 + ind2}>{ele}</p>;
+                    })}
+                  </div>
+                </div>
+              );
+            })}
+            <div ref={messagesEndRef} />
+          </div>
+        </div>
+        <div className="chatroom-typing-content">
+          <button className="type-controller">
+            <IconMenu icon="Attach" size={22} height={30} />
+          </button>
+          <textarea
+            ref={inputRef}
+            placeholder="Write a message..."
+            value={msg}
+            onKeyDown={(e: any) => handleKeyDown(e)}
+            onKeyUp={(e: any) => handleKeyUp(e)}
+            onChange={(e: any) => setMsg(e.target.value)}
+          />
+          <button className="type-controller">
+            <IconMenu icon="Send" size={20} />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ChatRoom;
